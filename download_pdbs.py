922f5ccc70ec8e58a369e7d7c9b4769fd9b291f6
Marek J <mjustyna@cs.put.poznan.pl>
2024-01-23 22:54:17 -0500

    Updated downloading script

--- download_pdbs.py
+++ download_pdbs.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 SOURCE_RNASOLO = 'https://rnasolo.cs.put.poznan.pl/api/query/structure?'
 SOURCE_RCSB = 'https://files.rcsb.org/download/'
-SOURCE = SOURCE_RCSB
+SOURCE = SOURCE_RNASOLO
 # download pdbs from rcsb.org using pdbid, model and chain
 # example: https://files.rcsb.org/download/6z9f_1.pdb
 
@@ -17,37 +17,54 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def download(pdbs, output_dir, skip_existing=True):
+def download(pdbs_chains, pdbs_models, output_dir, skip_existing=True):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    for pdb in tqdm(pdbs):
-        
-        pdb_id, model, chain = pdb
-        if skip_existing and os.path.exists(f'{output_dir}/{pdb_id}_{model}.pdb'):
-            continue
-        # url = f'{SOURCE}pdbid={pdb_id}&format=PDB&chains={chain}&models={model}'
-        url = f'{SOURCE}{pdb_id}.pdb{model}'
-        # os.system(f"curl '{url}' --output {output_dir}/{pdb_id}_{model}_{chain}.pdb")
-        
-        os.system(f"curl '{url}' --output {output_dir}/{pdb_id}_{model}.pdb")
-        # if downloaded file contains "Not Found" string, remove it and download again with cif format
-        with open(f'{output_dir}/{pdb_id}_{model}.pdb') as f:
-            file_content = f.read()
-            if 'Not Found' in file_content:
-                os.remove(f'{output_dir}/{pdb_id}_{model}.pdb')
-                url = f'{SOURCE}{pdb_id}.cif'
-                os.system(f"curl '{url}' --output {output_dir}/{pdb_id}.cif")
-                print(f'Not found: {pdb_id}_{model}')
+    for pdb_id, model in tqdm(pdbs_models.items()):
+        chains = pdbs_chains[pdb_id]
+        for chain in sorted(chains):
+            if skip_existing and os.path.exists(f'{output_dir}/{pdb_id}_{model}.pdb'):
                 continue
+            url = f'{SOURCE}pdbid={pdb_id}&format=PDB&chains={chain}&models={model}'
+
+            # url = f'{SOURCE}{pdb_id}.pdb{model}'
+            os.system(f"curl '{url}' --output {output_dir}/{pdb_id}_{model}_{chain}.pdb")
+            
+            # os.system(f"curl '{url}' --output {output_dir}/{pdb_id}_{model}.pdb")
+            # if downloaded file contains "Not Found" string, remove it and download again with cif format
+            # with open(f'{output_dir}/{pdb_id}_{model}.pdb') as f:
+            #     file_content = f.read()
+            #     if 'Not Found' in file_content:
+            #         os.remove(f'{output_dir}/{pdb_id}_{model}.pdb')
+            #         url = f'{SOURCE}{pdb_id}.cif'
+            #         os.system(f"curl '{url}' --output {output_dir}/{pdb_id}.cif")
+            #         print(f'Not found: {pdb_id}_{model}')
+            #         continue
+
+def load_all_pdb_ids():
+    pdb_ids = {}
+    with open('data/all_member_pdbids_all__3_300.txt') as f:
+        lines = f.readlines()
+    
+    for l in lines:
+        pdb_id = l.split("_")[0]
+        chains = l.split("_")[2].strip()
+        chains = sorted(chains.split("-"))
+        chains = "-".join(chains)
+        if pdb_id not in pdb_ids:
+            pdb_ids[pdb_id] = []
+        pdb_ids[pdb_id].append(chains)
+    return pdb_ids
 
 def main():
     args = parse_args()
     # load json file to dict
     with open(args.json) as f:
         json_file = json.load(f)
-    
-    pdb_to_download = []
-    print(len(json_file))
+    pdb_ids = load_all_pdb_ids()
+    print(f'All pdbs form txt: {len(pdb_ids)}')
+    pdbs_models = {}
+    print(f'Internal loops in Json: {len(json_file)}')
     for record in json_file:
         num_instances = record['num_instances']
         num_nucleotides = record['num_nucleotides']
@@ -57,13 +74,11 @@ def main():
             pdbs = record['alignment'][aln]
             pdb_id = pdbs[0].split('|')[0]
             model = pdbs[0].split('|')[1]
-            chain = pdbs[0].split('|')[2]
-            pdb = (pdb_id, model, chain)
-            pdb_to_download.append(pdb)
-    print(f'All instances: {len(pdb_to_download)}')
-    pdb_to_download = list(set(pdb_to_download)) # remove duplicates
-    print(f'Unique pdbs with chains: {len(pdb_to_download)}')
-    download(pdb_to_download, args.output_dir)
+            
+            pdbs_models[pdb_id] = model
+            
+    print(f'All pdbs ids in json: {len(pdbs_models)}')
+    download(pdb_ids, pdbs_models, args.output_dir)
 
 
 
