5a841dc0a69a1a819941dd6b994adb15a3865e9f
Marek J <mjustyna@cs.put.poznan.pl>
2024-01-25 13:35:47 -0500

    Updated command generation for RNA tools

--- process_for_RNAqua.py
+++ process_for_RNAqua.py
@@ -8,10 +8,11 @@ def parse_args():
     parser.add_argument('-i', '--input', type=str, help='Input file name')
     parser.add_argument('-d', '--directory', type=str, help='Process all files in the directory')
     parser.add_argument('-o', '--output_dir', type=str, help='Output directory')
+    parser.add_argument('--rna-tools', action='store_true', help='Prepare commands for rna-tools package')
     # parser.add_argument('-il', '--include_il', action='store_true', help='Include IL in the output')
     return parser.parse_args()
 
-def process_file(input_file, output_dir, il:bool=True, hl:bool=False, j3:bool=False):
+def process_file(input_file, output_dir, il:bool=True, hl:bool=False, j3:bool=False, rna_tools:bool=False):
     with open(input_file, 'r') as f:
         lines = f.readlines()
     
@@ -30,29 +31,38 @@ def process_file(input_file, output_dir, il:bool=True, hl:bool=False, j3:bool=Fa
         if pdb_name != pdb_name2:
             second_chain = pdb_name2[-1]
             pdb_name = f'{pdb_name}-{second_chain}'
-        seqs, rev_seqs, res_ids, rev_res_ids = extract_sequence(residues, begin_end)  # TODO: save the alignments with the sequences in fasta format
-        cmd = get_command(pdb_name, res_ids)
-        cmd2 = get_command(pdb_name, rev_res_ids, rev=True)
+        seqs, rev_seqs, res_ids, rev_res_ids = extract_sequence(
+                                                        residues,
+                                                        begin_end,
+                                                        rna_tools=rna_tools)  # TODO: save the alignments with the sequences in fasta format
+        cmd = get_command(pdb_name, res_ids, loop_name = name, rna_tools=rna_tools)
+        cmd2 = get_command(pdb_name, rev_res_ids, loop_name = name, rev=True, rna_tools=rna_tools)
         commands.append(cmd)
         commands.append(cmd2)
     return commands
 
-def process_directory(input_dir, output_dir):
+def process_directory(input_dir, output_dir, rna_tools:bool=False):
     dir_files = os.listdir(input_dir)
     commands = []
     for file in dir_files:
         if file.endswith('.csv'):
-            cmds = process_file(os.path.join(input_dir, file), output_dir)
+            cmds = process_file(os.path.join(input_dir, file), output_dir, rna_tools=rna_tools)
             commands.extend(cmds)
     return commands
 
-def extract_sequence(residues, begin_end):
+def extract_sequence(residues, begin_end, rna_tools:bool=False):
     sequence = [r.split('|')[3] for r in residues]
     res_nums = [r.split('|')[4] for r in residues]
     chains = [r.split('|')[2] for r in residues]
     seq = ''.join(sequence)
     seqs, bgend_pairs = split_sequence(seq, begin_end)
-    res_ids, rev_res_ids = get_residue_ids(res_nums, chains, bgend_pairs)
+    if rna_tools is not None:
+        res_ids, rev_res_ids = get_residue_ids_rna_tools(
+                                                        res_nums,
+                                                        chains,
+                                                        bgend_pairs)
+    else:
+        res_ids, rev_res_ids = get_residue_ids_aqua(res_nums, chains, bgend_pairs)
     rev_seqs = [s for s in seqs[::-1]]
     return seqs, rev_seqs, res_ids, rev_res_ids
 
@@ -66,22 +76,53 @@ def split_sequence(seq, begin_end):
         seqs.append(seq[be[0]:be[1]+1])
     return seqs, begin_end
 
-def get_residue_ids(res_nums, chains, bgend_pairs):
+def get_residue_ids_rna_tools(res_nums, chains, bgend_pairs):
+    strands = []
+    for a, b in bgend_pairs:
+        diff = b - a
+        ch = chains[a]
+        if len(ch) == 2:
+            ch = ch[1]
+        strands.append(f"{ch}:{res_nums[a]}-{res_nums[b]}>{ch}:1-{1+diff}")  # A:201-208>A:1-8
+    joined = ",".join(strands)
+    rev_joined = ",".join(strands[::-1])
+    return joined, rev_joined
+
+def get_residue_ids_aqua(res_nums, chains, bgend_pairs):
     strands = []
     for a, b in bgend_pairs:
         ch = chains[a]
         if len(ch) == 2:
             ch = ch[1]
-        strands.append(f"{ch}_{res_nums[a]},{(b-a)+1}")
+        strands.append(f"{ch}_{res_nums[a]},{(b-a)+1}") # 'A_24,7'
     joined = "|".join(strands)
     rev_joined = "|".join(strands[::-1])
     return joined, rev_joined
 
-def get_command(file_name, ids, rev:bool=False):
-    if rev:
-        command = f'docker-compose run --rm --entrypoint ./rnaqua rnaqua --command RENUMERATED-3D --single-model-file-path /tmp/mj/{file_name}.pdb --alignment "{file_name}.pdb:{ids}" --output-file-path /tmp/mj/{file_name}_m_rev.zip'
+def get_command(file_name, ids, loop_name, rev:bool=False, rna_tools:str=None):
+    if rna_tools is not None:
+        command = get_rna_tools_command(file_name, ids, loop_name=loop_name, rev=rev)
     else:
-        command = f'docker-compose run --rm --entrypoint ./rnaqua rnaqua --command RENUMERATED-3D --single-model-file-path /tmp/mj/{file_name}.pdb --alignment "{file_name}.pdb:{ids}" --output-file-path /tmp/mj/{file_name}_m.zip'
+        command = get_aqua_command(file_name, ids, loop_name=loop_name, rev=rev)
+    return command
+
+def get_aqua_command(file_name, ids, loop_name:str, rev:bool=False):
+    if rev:
+        command = f'docker-compose run --rm --entrypoint ./rnaqua rnaqua --command RENUMERATED-3D --single-model-file-path /tmp/mj/{file_name}.pdb --alignment "{file_name}.pdb:{ids}" --output-file-path /tmp/mj/{file_name}_{loop_name}_rev.zip'
+    else:
+        command = f'docker-compose run --rm --entrypoint ./rnaqua rnaqua --command RENUMERATED-3D --single-model-file-path /tmp/mj/{file_name}.pdb --alignment "{file_name}.pdb:{ids}" --output-file-path /tmp/mj/{file_name}_{loop_name}.zip'
+    return command
+
+def get_rna_tools_command(file_name, ids, loop_name:str, rev:bool=False):
+    """
+    Prepare commands as in example below:
+    `rna_pdb_tools.py --edit 'A:201-208>A:1-8,A:188-195>A:9-16' <pdb_file>.pdb >pdb_file_edited_<num>.pdb`
+
+    """
+    if rev:
+        command = f"rna_pdb_tools.py --edit '{ids}' pdbs/{file_name}.pdb > renumerated_pdbs/{file_name}_{loop_name}_rev.pdb"
+    else:
+        command = f"rna_pdb_tools.py --edit '{ids}' pdbs/{file_name}.pdb > renumerated_pdbs/{file_name}_{loop_name}.pdb"
     return command
 
 def write_commands(commands, output_dir):
@@ -95,10 +136,14 @@ def main():
         print('Please specify either input file or directory')
         return
     
+    if args.rna_tools is not None:
+        print("Preparing commands for rna-tools")
+    else:
+        print("Preparing commands for RNAqua")
     if args.input is not None:
-        cmds = process_file(args.input, args.output_dir)
+        cmds = process_file(args.input, args.output_dir, rna_tools = args.rna_tools)
     elif args.directory is not None:
-        cmds = process_directory(args.directory, args.output_dir)
+        cmds = process_directory(args.directory, args.output_dir, rna_tools = args.rna_tools)
     else:
         print('Please specify either input file or directory')
         return
