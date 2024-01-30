ccdb8db968fb7cb9d44222bea5b0d3710a55421f
Marek J <mjustyna@cs.put.poznan.pl>
2024-01-30 15:34:25 -0500

    RNA composer run

---
+++ run_composer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+for file in /tmp/mj/sbs_files/*.sbs
+do
+    filename=$(basename "$file" .sbs)
+    echo $filename
+    docker run --rm -e DB_HOST="172.17.0.1" -v /tmp/:/tmp/ --entrypoint ./rnacomposer ext-rnacomposer-engine -i/tmp/mj/RNAtools/$filename.fasta -usp/tmp/mj/sbs_files/$filename.sbs
+done
