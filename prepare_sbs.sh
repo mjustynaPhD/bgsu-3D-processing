f15af2f023072d6b9f3642c709985964539ee6d2
Marek J <mjustyna@cs.put.poznan.pl>
2024-01-30 16:10:03 -0500

    Update sbs generation

--- prepare_sbs.sh
+++ prepare_sbs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # for each file in the directory renumbered_pdbs
-for file in renumbered_pdbs/*.pdb
+for file in ren/*.zip
 do
     # get the filename without the extension
-    filename=$(basename "$file" .pdb)
+    filename=$(basename "$file" .zip)
     # run prepare_sbs.py on the file
     # python prepare_sbs.py $file
     # move the output file to the directory sbs_pdbs
     # mv $filename.sbs.pdb sbs_pdbs
     echo $filename
-    echo -e "1;1\nL1" > sbs_files/$filename.sbs; cat renumbered_pdbs/$filename.pdb >> sbs_files/$filename.sbs
+    echo -e "1;1\nL1" > sbs_files/$filename.sbs; cat ren/$filename/*.pdb >> sbs_files/$filename.sbs
 done
