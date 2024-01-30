65f6d0133f7c0e1dc871132ef328c4bea802c424
Marek J <mjustyna@cs.put.poznan.pl>
2024-01-30 16:07:27 -0500

    Updated shell scripts

--- run_composer.sh
+++ run_composer.sh
@@ -4,5 +4,5 @@ for file in /tmp/mj/sbs_files/*.sbs
 do
     filename=$(basename "$file" .sbs)
     echo $filename
-    docker run --rm -e DB_HOST="172.17.0.1" -v /tmp/:/tmp/ --entrypoint ./rnacomposer ext-rnacomposer-engine -i/tmp/mj/RNAtools/$filename.fasta -usp/tmp/mj/sbs_files/$filename.sbs
+    docker run --rm -e DB_HOST="172.17.0.1" -v /tmp/:/tmp/ --entrypoint ./rnacomposer ext-rnacomposer-engine -i/tmp/mj/RNAtools/$filename.dot -usp/tmp/mj/sbs_files/$filename.sbs
 done
--- unzip_all_files.sh
+++ unzip_all_files.sh
@@ -3,5 +3,7 @@
 # for each file in the directory renumbered_pdbs
 for file in /tmp/mj/ren/*.zip
 do
-    unzip $file -d /tmp/mj/ren
+    filename=$(basename "$file" .zip)
+    # unzip, if already unzipped. Save to given name
+    unzip -o $file -d /tmp/mj/unzip/$filename
 done
